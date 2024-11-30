545d60f919d400754a988f4efd95ab0040424dc2
小虎鲨 <3256149515@qq.com>
2024-11-30 22:26:29 +0800

    normalcommit-11.30

---
+++ front_page/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+<!--书写html结构-->
+  <div class="container">
+    <model-info/>
+    <model-train/>
+
+  </div>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+import ModelInfo from "@/components/modelInfo.vue";
+import ModelTrain from "@/components/modelTrain.vue";
+
+
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  height: 100vh;
+  position: relative;
+}
+</style>
+
+
---
+++ front_page/src/components/modelTrain.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="box"></div>
+</template>
+<script setup>
+
+</script>
+<style scoped>
+  .box{
+    background-color: #757575;
+    width: 400px;
+    height: 400px;
+    margin: 10px;
+  }
+</style>
